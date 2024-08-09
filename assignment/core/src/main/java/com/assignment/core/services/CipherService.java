0facd7bef320103b732cee853c6a490e8f22d0f5
Subha <subhachauhan9569@gmail.com>
2024-08-09 10:00:43 +0530

    modified changes

--- assignment/core/src/main/java/com/assignment/core/services/CipherService.java
+++ assignment/core/src/main/java/com/assignment/core/services/CipherService.java
@@ -1,8 +1,9 @@
 package com.assignment.core.services;
 
 import com.assignment.core.exceptions.EncryptionException;
+import com.assignment.core.exceptions.ProviderNotFoundException;
 
 public interface CipherService {
-    public String decrypt(String cipherText, String algorithm) throws EncryptionException;
-    public String encrypt(String cipherText, String algorithm) throws EncryptionException;
+    public String decrypt(String cipherText, String algorithm) throws EncryptionException, ProviderNotFoundException;
+    public String encrypt(String cipherText, String algorithm) throws EncryptionException, ProviderNotFoundException;
 }
--- assignment/core/src/main/java/com/assignment/core/services/impl/CipherServiceImpl.java
+++ assignment/core/src/main/java/com/assignment/core/services/impl/CipherServiceImpl.java
@@ -3,29 +3,38 @@ package com.assignment.core.services.impl;
 import com.assignment.core.exceptions.EncryptionException;
 import com.assignment.core.services.AsymmetricCipherProvider;
 import com.assignment.core.services.CipherService;
+
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.nio.file.ProviderNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
-@Component(service = CipherService.class)
+@Component(service = CipherService.class, immediate = true)
 public class CipherServiceImpl implements CipherService {
+
+    private static final Logger logger = LoggerFactory.getLogger(CipherServiceImpl.class);
 
     @Reference
     private List<AsymmetricCipherProvider> providers;
 
     @Override
-    public String encrypt(String plainText, String algorithm) throws EncryptionException {
+    public String encrypt(String plainText, String algorithm) throws EncryptionException, ProviderNotFoundException {
         AsymmetricCipherProvider provider = findProvider(algorithm)
-                .orElseThrow(() -> new EncryptionException("No provider found for: " + algorithm));
+                .orElseThrow(() ->
+                     new ProviderNotFoundException("No provider found for: " + algorithm));
         return provider.encrypt(plainText);
     }
 
     @Override
-    public String decrypt(String cipherText, String algorithm) throws EncryptionException {
+    public String decrypt(String cipherText, String algorithm) throws EncryptionException, ProviderNotFoundException {
+
         AsymmetricCipherProvider provider = findProvider(algorithm)
-                .orElseThrow(() -> new EncryptionException("No provider found for: " + algorithm));
+                .orElseThrow(() -> new ProviderNotFoundException("No provider found for: " + algorithm));
         return provider.decrypt(cipherText);
     }
 
@@ -35,4 +44,6 @@ public class CipherServiceImpl implements CipherService {
                 .findFirst();
     }
 }
+
+
 
