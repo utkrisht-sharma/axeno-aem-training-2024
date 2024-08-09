b5db4efca75d2164a8ef8f8f0b5ec859a3157955
Subha <subhachauhan9569@gmail.com>
2024-08-09 09:56:59 +0530

    added files for AES configuration

--- assignment/core/src/main/java/com/assignment/core/config/AESConfig.java
+++ assignment/core/src/main/java/com/assignment/core/config/AESConfig.java
@@ -1,5 +1,18 @@
 package com.assignment.core.config;
 
-public class AESConfig {
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.AttributeType;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+
+@ObjectClassDefinition(name = "AES Encryption Configuration", description = "Configuration for AES secret key")
+public @interface AESConfig {
+
+    @AttributeDefinition(
+            name = "Secret Key",
+            description = "Base64 encoded secret key",
+            type = AttributeType.STRING
+    )
+    String secretKey() default "";
+
 
 }
