9a0eeb4425f362f135c99b66f4f5200965b09a37
Shawarn Amunk <shawarnamunk@gmail.com>
2024-08-12 10:44:39 +0530

    Added sling model for custom breadcrumbs

---
+++ assignment/core/src/main/java/com/assignment/core/models/BreadcrumbModel.java
@@ -0,0 +1,51 @@
+package com.assignment.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Model(adaptables = Resource.class,defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class BreadcrumbModel {
+
+    @SlingObject
+    public Resource resource;
+
+    public List<Breadcrumb> breadcrumbs;
+
+    @PostConstruct
+    public void init() {
+        breadcrumbs = new ArrayList<>();
+        Breadcrumbs(resource, 0);
+    }
+
+    public void Breadcrumbs(Resource currentResource, int level) {
+        if (currentResource == null || level > 2) {
+            return;
+        }
+        String name = getResourceName(currentResource);
+        String path = currentResource.getPath();
+        breadcrumbs.add(new Breadcrumb(name, path));
+        Resource parent = currentResource.getParent();
+        if (parent != null) {
+            Breadcrumbs(parent, level + 1);
+        }
+    }
+
+    public String getResourceName(Resource resource) {
+        if (resource == null) {
+            return "";
+        }
+        return resource.getName();
+    }
+
+
+    public List<Breadcrumb> getBreadcrumbs() {
+        return breadcrumbs;
+    }
+}
