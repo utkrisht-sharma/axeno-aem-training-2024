3efc20728880f1553742bbac8ecb4c68da04a64f
psk10 <parthkashyap884@gmail.com>
2024-08-12 18:29:21 +0530

    modified Breadcrumb model

--- assignment/core/src/main/java/com/assignment/core/models/BreadcrumbModel.java
+++ assignment/core/src/main/java/com/assignment/core/models/BreadcrumbModel.java
@@ -1,10 +1,12 @@
 package com.assignment.core.models;
 
+import com.day.cq.wcm.api.Page;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.*;
 import org.apache.sling.models.annotations.injectorspecific.*;
 
+import java.awt.desktop.ScreenSleepEvent;
 import java.util.*;
 
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
@@ -16,21 +18,21 @@ public class BreadcrumbModel {
     @Self
     private Resource currentResource;
 
-    @ValueMapValue
-    private String activePage;
-
     public List<PathSegment> getNavigationPath() {
         List<PathSegment> segments = new LinkedList<>();
 
-        Resource pageResource = resolver.getResource(activePage);
+        Resource pageResource = currentResource;
+
         if (pageResource == null) {
             return segments;
         }
 
-        Resource parentResource = pageResource.getParent();
+        Resource currentPage = getCurrentPage(pageResource);
+
+        Resource parentResource = currentPage.getParent();
         int depth = 0;
 
-        while (parentResource != null && depth < 2) {
+        while (parentResource != null && depth < 3) {
             segments.add(0, new PathSegment(parentResource.getName(), parentResource.getPath()));
             parentResource = parentResource.getParent();
             depth++;
@@ -39,13 +41,24 @@ public class BreadcrumbModel {
         return segments;
     }
 
+    private Resource getCurrentPage(Resource currentResource){
+        while( !isPage(currentResource)){
+            currentResource= currentResource.getParent();
+        }
+        return currentResource;
+    }
+
+    private boolean isPage(Resource currentResource){
+        return "cq:Page".equals(currentResource.getValueMap().get("jcr:primaryType"));
+    }
+
     public static class PathSegment {
         private final String name;
         private final String path;
 
-        public PathSegment(String name, String url) {
+        public PathSegment(String name, String path) {
             this.name = name;
-            this.path = url;
+            this.path = path;
         }
 
         public String getName() {
