03c1a64e005c86199eddd81a666b47fd6e34c67b
psk10 <parthkashyap884@gmail.com>
2024-08-12 13:34:18 +0530

    added breadcrumbmodel

---
+++ assignment/core/src/main/java/com/assignment/core/models/BreadcrumbModel.java
@@ -0,0 +1,59 @@
+package org.myproject.core.components;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.*;
+import org.apache.sling.models.annotations.injectorspecific.*;
+
+import java.util.*;
+
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class BreadcrumbModel {
+
+    @SlingObject
+    private ResourceResolver resolver;
+
+    @Self
+    private Resource currentResource;
+
+    @ValueMapValue
+    private String activePage;
+
+    public List<PathSegment> getNavigationPath() {
+        List<PathSegment> segments = new LinkedList<>();
+
+        Resource pageResource = resolver.getResource(activePage);
+        if (pageResource == null) {
+            return segments;
+        }
+
+        Resource parentResource = pageResource.getParent();
+        int depth = 0;
+
+        while (parentResource != null && depth < 2) {
+            segments.add(0, new PathSegment(parentResource.getName(), parentResource.getPath()));
+            parentResource = parentResource.getParent();
+            depth++;
+        }
+
+        return segments;
+    }
+
+    public static class PathSegment {
+        private final String name;
+        private final String path;
+
+        public PathSegment(String name, String url) {
+            this.name = name;
+            this.path = url;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPath() {
+            return path;
+        }
+    }
+}
