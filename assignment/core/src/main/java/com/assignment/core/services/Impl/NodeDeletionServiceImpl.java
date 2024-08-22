d04dcdc4b3fd99b7266c3bf2b2ebac1ecee27b04
ShawarnAmunk <shawarnamunk@gmail.com>
2024-08-22 18:24:26 +0530

    modified file

--- assignment/core/src/main/java/com/assignment/core/services/Impl/NodeDeletionServiceImpl.java
+++ assignment/core/src/main/java/com/assignment/core/services/Impl/NodeDeletionServiceImpl.java
@@ -1,15 +1,9 @@
 package com.assignment.core.services.Impl;
 
-import com.assignment.core.config.NodeDeletionConfig;
 import com.assignment.core.services.NodeDeletionService;
 import org.apache.sling.api.resource.*;
-import org.apache.sling.commons.scheduler.ScheduleOptions;
-import org.apache.sling.commons.scheduler.Scheduler;
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.metatype.annotations.Designate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,10 +12,9 @@ import java.util.Collections;
 import java.util.Map;
 
 /**
- * Implementation of the  NodeDeletionService} interface that schedules and performs node deletion tasks.
+ * Implementation of the NodeDeletionService interface that deletes nodes under the specified root path.
  */
-@Component(service = Runnable.class, immediate = true)
-@Designate(ocd = NodeDeletionConfig.class)
+@Component(service = NodeDeletionService.class)
 public class NodeDeletionServiceImpl implements Runnable, NodeDeletionService {
 
     private static final Logger log = LoggerFactory.getLogger(NodeDeletionServiceImpl.class);
@@ -29,62 +22,13 @@ public class NodeDeletionServiceImpl implements Runnable, NodeDeletionService {
     @Reference
     private ResourceResolverFactory resourceResolverFactory;
 
-    @Reference
-    private Scheduler scheduler;
-
     private String rootPath;
-    private boolean schedulerEnabled;
-    private String schedulerName;
-    private String schedulerExpression;
 
-    /**
-     * Activates the component, initializes configuration settings, and schedules the node deletion job if enabled.
-     */
-    @Activate
-    protected void activate(NodeDeletionConfig config) {
-        this.rootPath = config.rootPath();
-        this.schedulerEnabled = config.schedulerEnabled();
-        this.schedulerName = config.schedulerName();
-        this.schedulerExpression = config.schedulerExpression();
-
-        log.info("NodeDeletionScheduler activated with rootPath: {}", rootPath);
-
-        if (schedulerEnabled) {
-            ScheduleOptions scheduleOptions = scheduler.EXPR(schedulerExpression);
-            scheduleOptions.name(schedulerName);
-            scheduler.schedule(this, scheduleOptions);
-            log.info("Scheduler scheduled with expression: {}", schedulerExpression);
-        } else {
-            log.info("Scheduler is disabled.");
-        }
-    }
-
-    /**
-     * Deactivates the component and unschedules the job if it was previously scheduled.
-     */
-    @Deactivate
-    protected void deactivate(NodeDeletionConfig config) {
-        if (schedulerEnabled) {
-            scheduler.unschedule(config.schedulerName());
-            log.info("Scheduler unscheduled.");
-        }
-    }
-
-    /**
-     * It deletes nodes under the specified root path if the scheduler is enabled.
-     */
     @Override
     public void run() {
-        if (schedulerEnabled) {
-            deleteNodes();
-        } else {
-            log.info("NodeDeletionScheduler is disabled.");
-        }
+        deleteNodes();
     }
 
-    /**
-     * Deletes nodes under the configured root path.
-     */
     @Override
     public void deleteNodes() {
         try (ResourceResolver resourceResolver = getServiceResourceResolver()) {
@@ -110,10 +54,6 @@ public class NodeDeletionServiceImpl implements Runnable, NodeDeletionService {
         }
     }
 
-
-    /**
-     * Provides a  ResourceResolver for the node deletion service.
-     */
     private ResourceResolver getServiceResourceResolver() throws LoginException {
         Map<String, Object> param = Collections.singletonMap(ResourceResolverFactory.SUBSERVICE, "nodeDeletionService");
         return resourceResolverFactory.getServiceResourceResolver(param);
