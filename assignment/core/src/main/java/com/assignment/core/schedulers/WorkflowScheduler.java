d2dc74486b374499ec1768561fd0842092cdcae3
Subha <subhachauhan9569@gmail.com>
2024-08-26 10:53:47 +0530

    modified files

--- assignment/core/src/main/java/com/assignment/core/schedulers/WorkflowScheduler.java
+++ assignment/core/src/main/java/com/assignment/core/schedulers/WorkflowScheduler.java
@@ -1,156 +1,156 @@
-package com.assignment.core.schedulers;
-
-import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.api.resource.ResourceResolverFactory;
-import org.apache.sling.commons.scheduler.ScheduleOptions;
-import org.apache.sling.commons.scheduler.Scheduler;
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
-import org.osgi.service.component.annotations.Modified;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.metatype.annotations.Designate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.adobe.granite.workflow.WorkflowSession;
-import com.adobe.granite.workflow.exec.WorkflowData;
-import com.adobe.granite.workflow.model.WorkflowModel;
-import com.assignment.core.config.WorkflowSchedulerConfiguration;
-
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * Scheduler component to manage and execute workflow.
- *
- */
-@Component(
-        name = "Workflow Scheduler",
-        service = Runnable.class,
-        immediate = true)
-@Designate(ocd = WorkflowSchedulerConfiguration.class)
-public class WorkflowScheduler implements Runnable {
-
-    private final Logger logger = LoggerFactory.getLogger(WorkflowScheduler.class);
-
-
-    @Reference
-    private Scheduler scheduler;
-
-    @Reference
-    private ResourceResolverFactory resourceResolverFactory;
-
-    private String csvPath;
-    private int schedulerId;
-    private String model;
-
-    /**
-     * Activates the scheduler with the provided configuration.
-     *
-     * @param configuration The configuration to apply.
-     */
-    @Activate
-    private void activate(WorkflowSchedulerConfiguration configuration) {
-        logger.info("Activating Workflow Scheduler with configuration: {}", configuration);
-        this.csvPath = configuration.csvPath();
-        this.schedulerId = configuration.schedulerName().hashCode();
-        this.model = configuration.model();
-        addScheduler(configuration);
-    }
-
-    /**
-     * Updates the scheduler with new configuration.
-     *
-     * @param configuration The updated configuration.
-     */
-    @Modified
-    protected void modified(WorkflowSchedulerConfiguration configuration) {
-        logger.info("Modifying Workflow Scheduler with new configuration: {}", configuration);
-        removeScheduler();
-        this.csvPath = configuration.csvPath();
-        this.schedulerId = configuration.schedulerName().hashCode();
-        this.model = configuration.model();
-        addScheduler(configuration);
-    }
-
-    /**
-     * Deactivates and removes the scheduler.
-     *
-     */
-    @Deactivate
-    protected void deactivate() {
-        logger.info("Deactivating Workflow Scheduler.");
-        removeScheduler();
-    }
-
-    /**
-     * Adds a scheduler with the specified configuration.
-     *
-     * @param config The configuration to use for scheduling.
-     */
-    private void addScheduler(WorkflowSchedulerConfiguration config) {
-        boolean enabled = config.enabled();
-        if (enabled) {
-            ScheduleOptions scheduleOptions = scheduler.EXPR(config.cronExpression());
-            scheduleOptions.name(String.valueOf(schedulerId));
-            scheduleOptions.canRunConcurrently(false);
-            scheduler.schedule(this, scheduleOptions);
-            logger.info("Workflow Scheduler added successfully with name: {}", schedulerId);
-
-            ScheduleOptions scheduleOptionsNow = scheduler.NOW();
-            scheduler.schedule(this, scheduleOptionsNow);
-        } else {
-            logger.info("Workflow Scheduler is in disabled state.");
-        }
-    }
-
-    /**
-     * Removes the scheduler.
-     *
-     */
-    private void removeScheduler() {
-        logger.info("Removing Workflow Scheduler with name: {}", schedulerId);
-        scheduler.unschedule(String.valueOf(schedulerId));
-    }
-
-    /**
-     * Executes the workflow based on the configuration.
-     */
-    @Override
-    public void run() {
-        ResourceResolver resourceResolver = null;
-        try {
-            Map<String, Object> param = new HashMap<>();
-            param.put(ResourceResolverFactory.SUBSERVICE, "workflowservice");
-            resourceResolver = resourceResolverFactory.getServiceResourceResolver(param);
-
-            if (resourceResolver != null) {
-                logger.info("ResourceResolver obtained successfully.");
-
-                WorkflowSession workflowSession = resourceResolver.adaptTo(WorkflowSession.class);
-                if (workflowSession != null) {
-                    WorkflowModel workflowModel = workflowSession.getModel(model);
-                    if (workflowModel != null) {
-                        WorkflowData workflowData = workflowSession.newWorkflowData("JCR_PATH", csvPath);
-                        workflowSession.startWorkflow(workflowModel, workflowData);
-                        logger.info("Workflow started successfully with model: {} for path: {}", workflowModel.getTitle(), csvPath);
-                    } else {
-                        logger.error("Workflow model not found: {}", model);
-                    }
-                } else {
-                    logger.error("Unable to adapt ResourceResolver to WorkflowSession.");
-                }
-            } else {
-                logger.error("Failed to obtain ResourceResolver.");
-            }
-        } catch (Exception e) {
-            logger.error("Error during workflow execution", e);
-        } finally {
-            if (resourceResolver != null) {
-                resourceResolver.close();
-                logger.info("ResourceResolver closed.");
-            }
-        }
-    }
-}
+//package com.assignment.core.schedulers;
+//
+//import org.apache.sling.api.resource.ResourceResolver;
+//import org.apache.sling.api.resource.ResourceResolverFactory;
+//import org.apache.sling.commons.scheduler.ScheduleOptions;
+//import org.apache.sling.commons.scheduler.Scheduler;
+//import org.osgi.service.component.annotations.Activate;
+//import org.osgi.service.component.annotations.Component;
+//import org.osgi.service.component.annotations.Deactivate;
+//import org.osgi.service.component.annotations.Modified;
+//import org.osgi.service.component.annotations.Reference;
+//import org.osgi.service.metatype.annotations.Designate;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//import com.adobe.granite.workflow.WorkflowSession;
+//import com.adobe.granite.workflow.exec.WorkflowData;
+//import com.adobe.granite.workflow.model.WorkflowModel;
+//import com.assignment.core.config.WorkflowSchedulerConfiguration;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+///**
+// * Scheduler component to manage and execute workflow.
+// *
+// */
+//@Component(
+//        name = "Workflow Scheduler",
+//        service = Runnable.class,
+//        immediate = true)
+//@Designate(ocd = WorkflowSchedulerConfiguration.class)
+//public class WorkflowScheduler implements Runnable {
+//
+//    private final Logger logger = LoggerFactory.getLogger(WorkflowScheduler.class);
+//
+//
+//    @Reference
+//    private Scheduler scheduler;
+//
+//    @Reference
+//    private ResourceResolverFactory resourceResolverFactory;
+//
+//    private String csvPath;
+//    private int schedulerId;
+//    private String model;
+//
+//    /**
+//     * Activates the scheduler with the provided configuration.
+//     *
+//     * @param configuration The configuration to apply.
+//     */
+//    @Activate
+//    private void activate(WorkflowSchedulerConfiguration configuration) {
+//        logger.info("Activating Workflow Scheduler with configuration: {}", configuration);
+//        this.csvPath = configuration.csvPath();
+//        this.schedulerId = configuration.schedulerName().hashCode();
+//        this.model = configuration.model();
+//        addScheduler(configuration);
+//    }
+//
+//    /**
+//     * Updates the scheduler with new configuration.
+//     *
+//     * @param configuration The updated configuration.
+//     */
+//    @Modified
+//    protected void modified(WorkflowSchedulerConfiguration configuration) {
+//        logger.info("Modifying Workflow Scheduler with new configuration: {}", configuration);
+//        removeScheduler();
+//        this.csvPath = configuration.csvPath();
+//        this.schedulerId = configuration.schedulerName().hashCode();
+//        this.model = configuration.model();
+//        addScheduler(configuration);
+//    }
+//
+//    /**
+//     * Deactivates and removes the scheduler.
+//     *
+//     */
+//    @Deactivate
+//    protected void deactivate() {
+//        logger.info("Deactivating Workflow Scheduler.");
+//        removeScheduler();
+//    }
+//
+//    /**
+//     * Adds a scheduler with the specified configuration.
+//     *
+//     * @param config The configuration to use for scheduling.
+//     */
+//    private void addScheduler(WorkflowSchedulerConfiguration config) {
+//        boolean enabled = config.enabled();
+//        if (enabled) {
+//            ScheduleOptions scheduleOptions = scheduler.EXPR(config.cronExpression());
+//            scheduleOptions.name(String.valueOf(schedulerId));
+//            scheduleOptions.canRunConcurrently(false);
+//            scheduler.schedule(this, scheduleOptions);
+//            logger.info("Workflow Scheduler added successfully with name: {}", schedulerId);
+//
+//            ScheduleOptions scheduleOptionsNow = scheduler.NOW();
+//            scheduler.schedule(this, scheduleOptionsNow);
+//        } else {
+//            logger.info("Workflow Scheduler is in disabled state.");
+//        }
+//    }
+//
+//    /**
+//     * Removes the scheduler.
+//     *
+//     */
+//    private void removeScheduler() {
+//        logger.info("Removing Workflow Scheduler with name: {}", schedulerId);
+//        scheduler.unschedule(String.valueOf(schedulerId));
+//    }
+//
+//    /**
+//     * Executes the workflow based on the configuration.
+//     */
+//    @Override
+//    public void run() {
+//        ResourceResolver resourceResolver = null;
+//        try {
+//            Map<String, Object> param = new HashMap<>();
+//            param.put(ResourceResolverFactory.SUBSERVICE, "workflowservice");
+//            resourceResolver = resourceResolverFactory.getServiceResourceResolver(param);
+//
+//            if (resourceResolver != null) {
+//                logger.info("ResourceResolver obtained successfully.");
+//
+//                WorkflowSession workflowSession = resourceResolver.adaptTo(WorkflowSession.class);
+//                if (workflowSession != null) {
+//                    WorkflowModel workflowModel = workflowSession.getModel(model);
+//                    if (workflowModel != null) {
+//                        WorkflowData workflowData = workflowSession.newWorkflowData("JCR_PATH", csvPath);
+//                        workflowSession.startWorkflow(workflowModel, workflowData);
+//                        logger.info("Workflow started successfully with model: {} for path: {}", workflowModel.getTitle(), csvPath);
+//                    } else {
+//                        logger.error("Workflow model not found: {}", model);
+//                    }
+//                } else {
+//                    logger.error("Unable to adapt ResourceResolver to WorkflowSession.");
+//                }
+//            } else {
+//                logger.error("Failed to obtain ResourceResolver.");
+//            }
+//        } catch (Exception e) {
+//            logger.error("Error during workflow execution", e);
+//        } finally {
+//            if (resourceResolver != null) {
+//                resourceResolver.close();
+//                logger.info("ResourceResolver closed.");
+//            }
+//        }
+//    }
+//}
--- assignment/core/src/main/java/com/assignment/core/workflows/CSVPageCreationWorkflowProcess.java
+++ assignment/core/src/main/java/com/assignment/core/workflows/CSVPageCreationWorkflowProcess.java
@@ -5,18 +5,15 @@ import com.adobe.granite.workflow.exec.WorkItem;
 import com.adobe.granite.workflow.exec.WorkflowProcess;
 import com.adobe.granite.workflow.metadata.MetaDataMap;
 import com.day.cq.dam.api.Asset;
-import com.day.cq.dam.api.Rendition;
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
 import com.day.cq.wcm.api.WCMException;
-import org.apache.commons.logging.Log;
 import org.apache.sling.api.resource.*;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -25,9 +22,6 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * Workflow process for creating pages from a CSV file in DAM.
- */
 @Component(service = WorkflowProcess.class, property = {"process.label=CSV Page Creation Process"})
 public class CSVPageCreationWorkflowProcess implements WorkflowProcess {
 
@@ -36,124 +30,132 @@ public class CSVPageCreationWorkflowProcess implements WorkflowProcess {
     @Reference
     private ResourceResolverFactory resourceResolverFactory;
 
-    /**
-     * Executes the workflow process to create pages from a CSV file.
-     *
-     * @param workItem The work item containing the workflow data.
-     * @param workflowSession The session used to execute the workflow.
-     * @param metaDataMap Metadata associated with the workflow.
-     */
     @Override
     public void execute(WorkItem workItem, WorkflowSession workflowSession, MetaDataMap metaDataMap) {
-        ResourceResolver resourceResolver = null;
-        try {
+        try (ResourceResolver resourceResolver = workflowSession.adaptTo(ResourceResolver.class)) {
             String payloadPath = workItem.getWorkflowData().getPayload().toString();
-            resourceResolver = workflowSession.adaptTo(ResourceResolver.class);
+            if (resourceResolver != null) {
+                log.info("Starting process for payload: {}", payloadPath);
+                processPayload(resourceResolver, payloadPath);
+            } else {
+                log.error("ResourceResolver could not be obtained.");
+            }
+        } catch (RepositoryException | WCMException | IOException e) {
+            log.error("Error during CSV Page Creation Workflow Process", e);
+        }
+    }
 
-            Resource resource = resourceResolver.getResource(payloadPath);
-            if (resource != null) {
-                log.info("Resource found at path: {}, Resource Type: {}", payloadPath, resource.getResourceType());
-
-                Asset asset = resource.adaptTo(Asset.class);
-                if (asset != null) {
-                    Rendition original = asset.getOriginal();
-                    InputStream inputStream = original.adaptTo(InputStream.class);
-
+    /**
+     * Processes the payload and initiates CSV processing.
+     *
+     * @param resourceResolver The resource resolver.
+     * @param payloadPath  path of csv as payload.
+     */
+    private void processPayload(ResourceResolver resourceResolver, String payloadPath) throws IOException, RepositoryException, WCMException {
+        log.info("Processing payload at path: {}", payloadPath);
+        Resource resource = resourceResolver.getResource(payloadPath);
+        if (resource != null) {
+            Asset asset = resource.adaptTo(Asset.class);
+            if (asset != null) {
+                try (InputStream inputStream = asset.getOriginal().adaptTo(InputStream.class)) {
                     if (inputStream != null) {
                         log.info("CSV file loaded successfully, starting to process the file.");
                         processCSV(inputStream, resourceResolver);
                     } else {
                         log.error("Failed to obtain InputStream from the asset's original rendition.");
                     }
-                } else {
-                    log.error("Resource at path is not a DAM Asset: {}", payloadPath);
                 }
             } else {
-                log.error("Resource not found at path: {}", payloadPath);
+                log.error("Resource at path is not a DAM Asset: {}", payloadPath);
             }
-        } catch (RepositoryException | WCMException | IOException e) {
-            log.error("Error during CSV Page Creation Workflow Process", e);
-        } finally {
-            if (resourceResolver != null) {
-                resourceResolver.close();
-            }
+        } else {
+            log.error("Resource not found at path: {}", payloadPath);
         }
     }
 
     /**
-     * Processes the CSV file to create pages.
+     * Processes the CSV file and creates pages.
      *
-     * @param inputStream The InputStream of the CSV file.
-     * @param resourceResolver The resource resolver for page creation.
-     * @throws Exception If an error occurs during CSV processing.
+     * @param inputStream      The InputStream of the CSV file.
+     * @param resourceResolver The resource resolver.
      */
     private void processCSV(InputStream inputStream, ResourceResolver resourceResolver) throws IOException, RepositoryException, WCMException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        String line;
-        boolean isFirstLine = true;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            boolean isFirstLine = true;
 
-        PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
-        if (pageManager == null) {
-            log.error("PageManager is not available.");
-            return;
-        }
-
-        while ((line = reader.readLine()) != null) {
-            if (isFirstLine) {
-                isFirstLine = false;
-                continue;
+            PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+            if (pageManager == null) {
+                log.error("PageManager is not available.");
+                return;
             }
 
-            List<String> values = Arrays.asList(line.split(","));
-            if (values.size() < 5) {
-                log.error("Invalid CSV line, not enough columns: {}", line);
-                continue;
+            log.info("Starting to read the CSV file.");
+            while ((line = reader.readLine()) != null) {
+                if (isFirstLine) {
+                    isFirstLine = false;
+                    continue;
+                }
+
+                List<String> values = Arrays.asList(line.split(","));
+                if (values.size() < 5) {
+                    log.error("Invalid CSV line, not enough columns: {}", line);
+                    continue;
+                }
+
+                String title = values.get(0).trim();
+                String path = values.get(1).trim();
+                String description = values.get(2).trim();
+                String tags = values.get(3).trim();
+                String thumbnail = values.get(4).trim();
+
+                log.info("Processing page with title: {}, path: {}", title, path);
+
+                String validPageName = createValidPageName(title);
+
+                createPage(pageManager, resourceResolver, path, validPageName, title, description, tags, thumbnail);
             }
-
-            String title = values.get(0).trim();
-            String path = values.get(1).trim();
-            String description = values.get(2).trim();
-            String tags = values.get(3).trim();
-            String thumbnail = values.get(4).trim();
-
-            String validPageName = createValidPageName(title);
-
-            createPage(pageManager, path, validPageName, title, description, tags, thumbnail);
         }
     }
 
     /**
-     * Creates a valid page name from the given title.
+     * Creates a valid page name from the title.
      *
      */
     private String createValidPageName(String title) {
         return title.replaceAll("[^\\p{Alnum}_]", "-");
+
     }
 
     /**
-     * Creates a page with the given details.
+     * Creates a page and sets its properties, including a thumbnail.
      *
-     * @param pageManager  PageManager to create the page.
-     * @param path  path where the page will be created.
-     * @param pageName  name of the page.
-     * @param title  title of the page.
-     * @param description  description of the page.
-     * @param tags  tags for the page.
-     * @param thumbnail thumbnail path for the page from asset.
      */
-    private void createPage(PageManager pageManager, String path, String pageName, String title, String description, String tags, String thumbnail)
-            throws RepositoryException , WCMException {
+    private void createPage(PageManager pageManager, ResourceResolver resourceResolver, String path, String pageName, String title, String description, String tags, String thumbnail)
+            throws WCMException, PersistenceException {
 
         Page page = pageManager.create(path, pageName, "/conf/global/settings/wcm/templates/page", title);
         if (page != null) {
-            Node contentNode = page.getContentResource().adaptTo(Node.class);
-            if (contentNode != null) {
-                contentNode.setProperty("jcr:description", description);
-                contentNode.setProperty("cq:tags", tags.split(";"));
-                contentNode.setProperty("cq:templateThumbnail", thumbnail);
-                log.info("Page created at: {}", page.getPath());
+            log.info("Page created successfully at: {}", page.getPath());
+            Resource contentResource = page.getContentResource();
+            ModifiableValueMap properties = contentResource.adaptTo(ModifiableValueMap.class);
+
+            if (properties != null) {
+                properties.put("jcr:description", description);
+                properties.put("cq:tags", tags.split(";"));
+
+                // Creating a node for storing the thumbnail image
+                Resource thumbnailResource = resourceResolver.create(contentResource, "thumbnail", ValueMap.EMPTY);
+                ModifiableValueMap thumbnailProps = thumbnailResource.adaptTo(ModifiableValueMap.class);
+                if (thumbnailProps != null) {
+                    thumbnailProps.put("jcr:primaryType", "nt:unstructured");
+                    thumbnailProps.put("fileReference", thumbnail);
+                    log.info("Thumbnail image set at: {}", thumbnailResource.getPath());
+                }
             }
+            resourceResolver.commit();
+        } else {
+            log.error("Failed to create page at path: {}", path);
         }
     }
-
 }
