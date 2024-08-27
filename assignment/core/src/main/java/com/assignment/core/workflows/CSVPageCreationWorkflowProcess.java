0b360f5cad281b45336820fbeeb95a9d93e17929
Subha <subhachauhan9569@gmail.com>
2024-08-28 00:52:16 +0530

    modified file

--- assignment/core/src/main/java/com/assignment/core/workflows/CSVPageCreationWorkflowProcess.java
+++ assignment/core/src/main/java/com/assignment/core/workflows/CSVPageCreationWorkflowProcess.java
@@ -5,13 +5,17 @@ import com.adobe.granite.workflow.WorkflowSession;
 import com.adobe.granite.workflow.exec.WorkItem;
 import com.adobe.granite.workflow.exec.WorkflowProcess;
 import com.adobe.granite.workflow.metadata.MetaDataMap;
+import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Workflow process for creating pages from a CSV file.
@@ -24,6 +28,10 @@ public class CSVPageCreationWorkflowProcess implements WorkflowProcess {
     @Reference
     private ProcessCSVService processCSVService;
 
+    @Reference
+    private ResourceResolverFactory resourceResolverFactory;
+
+
     /**
      * Executes the workflow process to create pages from a CSV file.
      *
@@ -33,7 +41,7 @@ public class CSVPageCreationWorkflowProcess implements WorkflowProcess {
      */
     @Override
     public void execute(WorkItem workItem, WorkflowSession workflowSession, MetaDataMap metaDataMap) {
-        try (ResourceResolver resourceResolver = workflowSession.adaptTo(ResourceResolver.class)) {
+        try (ResourceResolver resourceResolver = getServiceResourceResolver()) {
             String payloadPath = workItem.getWorkflowData().getPayload().toString();
             if (resourceResolver != null) {
                 log.info("Starting process for payload: {}", payloadPath);
@@ -48,6 +56,14 @@ public class CSVPageCreationWorkflowProcess implements WorkflowProcess {
             }
         } catch (IOException e) {
             log.error("Error during CSV Page Creation Workflow Process", e);
+        } catch (LoginException e) {
+            log.error("Error during getting resolver", e);
         }
+    }
+
+    private ResourceResolver getServiceResourceResolver() throws LoginException {
+        Map<String, Object> param = new HashMap<>();
+        param.put(ResourceResolverFactory.SUBSERVICE, "workflowserviceuser");
+        return resourceResolverFactory.getServiceResourceResolver(param);
     }
 }
